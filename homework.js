204267ffb49eba52cea8ca1e86a98d69c02b9987
AngieCana <grilozangie@gmail.com>
2023-06-15 19:20:59 -0400

    second commit

---
+++ homework.js
@@ -0,0 +1,11 @@
+class Hamster{
+  constructor(name){
+    this.name = name;
+    this.owner = '';
+    this.wheelRun = "squeak squeak";
+    this.eatFood = "nibble nibble"
+    this.price = 15;
+  }
+}
+
+console.log(Hamster);
